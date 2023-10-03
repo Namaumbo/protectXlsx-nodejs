f413311b0712e6c6c14afe0425fde74ef7115d91
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2023-10-03 23:34:28 +0200

    sent-protected

--- app.js
+++ app.js
@@ -1,66 +1,70 @@
 const express = require("express");
 const XlsxPopulate = require("xlsx-populate");
-const fileUpload = require("express-fileupload");
+// const fileUpload = require("express-fileupload");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const { parse, stringify, toJSON, fromJSON } = require("flatted");
+
 const app = express();
-app.use(fileUpload());
+// app.use(fileUpload());
 
-// app.post("/api/change", async (req, res) => {
-//   try {
-//     const excelBuffer = req.body;
-//     const workbook = await XlsxPopulate.fromBlankAsync(excelBuffer);
-//     workbook
-//       .then((workbook) => {
-//         workbook.toFileAsync("./example.xlsx", { password: "123456" });
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
 
-//     const protectedExcelBuffer = await workbook.outputAsync();
+const storage = multer.diskStorage({
+  destination:(req, res, cb) =>{
+    const tempDir = 'uploads';
+    fs.mkdirSync(tempDir, { recursive: true });
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueFileName =  file.filename  + path.extname(file.originalname);
+    cb(null, uniqueFileName);
+  },
+});
 
-//     res.setHeader(
-//       "Content-Disposition",
-//       "attachment; filename=protected_excel.xlsx"
-//     );
-//     res.setHeader(
-//       "Content-Type",
-//       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-//     );
-//     res.send(protectedExcelBuffer);
-//   } catch (error) {
-//     console.error("Error protecting the Excel file:", error);
-//     res.status(500).send("Internal Server Error");
-//   }
+const upload = multer({ storage });
 
-//   // workbook
-//   //   .then((workbook) => {
-//   //     res.setHeader(
-//   //       "Content-Disposition",
-//   //       "attachment; filename=protected_excel.xlsx"
-//   //     );
-//   //     res.setHeader(
-//   //       "Content-Type",
-//   //       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-//   //     );
-//   //     res.send(workbook.toFileAsync("./example.xlsx", { password: "123456" }));
-//   //   })
-//   //   .catch((err) => {
-//   //     console.log("error processing workbook");
-//   //   });
-// });
+app.post("/api/change",upload.single('file'), async (req, res) => {
+  try {
+   
+    const excelBuffer = req.file.path; 
+    console.log(excelBuffer);  
+    const workbook = XlsxPopulate.fromFileAsync(excelBuffer);
+   const name = excelBuffer.split('/')
+   console.log(name)
+    workbook.then((book) => {
+      book.toFileAsync('here.xlsx', { password: "123" });
+      res.setHeader(
+        "Content-Disposition",
+        "attachment; filename=protected_excel.xlsx"
+      );
+      res.setHeader(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      );
+      const file = path.join(__dirname, "here.xlsx");
+      res.status(200).sendFile(file);
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error.message);
+  }
+});
 
-// app.listen(3000, (req, res) => {
-//   console.log("listening on port 3000");
-// });
+app.listen(3000, (req, res) => {
+  console.log("listening on port 3000");
+});
 
-// =-----------------main logic --------------------
-const workbook = XlsxPopulate.fromBlankAsync("./example.xlsx");
+// ------------------------------------main logic ---------------------------------------------
+// const workbook = await XlsxPopulate.fromBlankAsync("./here.xlsx");
 
-workbook
-  .then((workbook) => {
-    workbook.toFileAsync("./protectFile.xlsx", { password: "qwer" });
-    return workbook;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// // workbook
+// //   .then((workbook) => {
+// //     console.log(workbook);
+
+//   await    workbook.toFileAsync("protectFiles.xlsx", { password: "qwer" });
+//     return workbook;
+//   // })
+//   // .catch((err) => {
+//   //   console.log(err);
+//   // });
